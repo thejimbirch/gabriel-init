f17428854ddf0f8d27ab179b2160042365ba86ba
Jim Birch <hello@jimbir.ch>
2017-09-21 17:28:11 -0500

    Adds npm pa11y
    pa11y@beta for Headless Chrome use, not PhantomJS.  Will probably just be `pally` by the time you get a new mac.

--- install.sh
+++ install.sh
@@ -116,6 +116,7 @@ npm install --global grunt-cli # The Grunt command line interface.
 npm install --global imageoptim-cli # Automates ImageOptim, ImageAlpha, and JPEGmini.
 npm install --global imagemin-cli # Minify images.
 npm install --global lighthouse # Lighthouse analyzes web apps and web pages, collecting modern performance metrics and insights on developer best practices.
+npm install --global pa11y@beta # Pa11y is your automated accessibility testing pal. It runs HTML CodeSniffer from the command line for programmatic accessibility reporting.
 npm install --global prettier # Prettier is an opinionated code formatter.
 npm install --global psi # PageSpeed Insights with reporting.
 npm install --global sitespeed.io # Using sitespeed.io you can test your web site against Web Performance best practices.
